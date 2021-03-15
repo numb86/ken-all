9d4c2d1cc3fd450e9ffe40ae3b9d94296b9cfde1
ryym <ryym.64@gmail.com>
2021-03-15 10:42:08 +0900

    feat: global.Promise の上書きは必要な時のみ行う
    ネイティブの Promise がサポートされてる環境で ken-all を使う場合、
    ken-all をロードする前後で global.Promise が変わってしまうのを避けたい。
    
    なお es6-promise が必要かどうかは実行時にしかわからないので、
    require が if 文の中にあっても webpack のビルド結果には依然含まれる。
    本当はポリフィルのロードをライブラリユーザ側の責務としたいところ。

--- src/fetchCsv.ts
+++ src/fetchCsv.ts
@@ -1,7 +1,9 @@
 const Promise = require('es6-promise');
 const {fetch} = require('fetch-ponyfill')(Promise);
 
-global.Promise = Promise;
+if (typeof global.Promise === 'undefined') {
+  global.Promise = Promise;
+}
 
 const fetchCsv = (postCodeFront: string): Promise<Response> =>
   fetch(`https://ken-all.numb86.net/csv/${postCodeFront}.csv`);
