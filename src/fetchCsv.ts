e8e7c2122a2774b04de8b5afa74fc1cc5c03b2a9
numb86 <16703337+numb86@users.noreply.github.com>
2021-05-07 11:14:29 +0900

    feat!: ライブラリ側が fetch や Promise のポリフィルを用意するのを止める (#67)
    * feat!: ライブラリ側が fetch や Promise のポリフィルを用意するのを止める
    
    * test: Node.js で動作確認を行うテストを削除する
    
    * docs: ドキュメントに、fetch のポリフィルが必要であることを書く

--- src/fetchCsv.ts
+++ src/fetchCsv.ts
@@ -1,10 +1,3 @@
-const Promise = require('es6-promise');
-const {fetch} = require('fetch-ponyfill')(Promise);
-
-if (typeof global.Promise === 'undefined') {
-  global.Promise = Promise;
-}
-
 const fetchCsv = (postCodeFront: string): Promise<Response> =>
   fetch(`https://ken-all.numb86.net/csv/${postCodeFront}.csv`);
 
