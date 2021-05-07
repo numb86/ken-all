9f08e23df0fa635c01eed4ef9acc98e4ab897462
numb86 <16703337+numb86@users.noreply.github.com>
2021-05-07 13:07:44 +0900

    fix: cors を有効にする

--- src/fetchCsv.ts
+++ src/fetchCsv.ts
@@ -1,4 +1,4 @@
 const fetchCsv = (postCodeFront: string): Promise<Response> =>
-  fetch(`https://ken-all.numb86.net/csv/${postCodeFront}.csv`);
+  fetch(`https://ken-all.numb86.net/csv/${postCodeFront}.csv`, {mode: 'cors'});
 
 export default fetchCsv;
