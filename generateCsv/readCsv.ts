de06de68f729635a2fdedf9c42ba358beea13f07
numb86 <16703337+numb86@users.noreply.github.com>
2025-12-29 04:30:50 +0900

    chore: TypeScript 版の CSV 生成スクリプトを削除する

--- generateCsv/readCsv.ts
+++
@@ -1,9 +0,0 @@
-const fs = require('fs').promises;
-// eslint-disable-next-line import/no-extraneous-dependencies
-const iconv = require('iconv-lite');
-
-const readCsv = async (path: string): Promise<string> => {
-  const buf: Buffer = await fs.readFile(path);
-  return iconv.decode(buf, 'shiftjis');
-};
-export default readCsv;
